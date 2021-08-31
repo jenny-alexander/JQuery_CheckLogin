8fa176688755564a0ea913555d2d5552dd906d41
Jennifer Alexander <jenny_alexander@icloud.com>
2021-08-31 14:59:16 -0500

    complete shell functionality

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,32 @@
 console.log(`js`);
+
+//JQ handshake
+$( document ).ready( onReady );
+
+function onReady() {
+    console.log(`JQ is ready`);
+
+    //Button to validate username and password
+    $( '#validateInput' ).on( 'click', checkInput );
+}
+
+function checkInput() {
+
+    let email = $( '#email' ).val();
+    console.log(`email:`,email);
+    let pwd = $( '#password' ).val();
+    console.log(`password:`,pwd);
+
+    let result = "Great! you can enter.";
+    outputResult( result );
+}
+
+function outputResult( result ) {
+
+    console.log(result);
+    let elCheckResult = $( '#checkResult' );
+    elCheckResult.empty();
+    elCheckResult.append( result );
+
+}
+
